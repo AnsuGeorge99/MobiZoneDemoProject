d447954ba89223c576a10c55f92e1c6048b6ec20
Ansu George <ansususygeorge@gmail.com>
2022-07-18 12:44:59 +0530

    validation added in ForgotPassword.cshtml

--- DomainLayer/ForgotPassword.cs
+++ DomainLayer/ForgotPassword.cs
@@ -13,7 +13,7 @@ namespace DomainLayer
         public int userId { get; set; }
         [Required(ErrorMessage = "*Email Address is required")]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z\s]+\.[a-zA-Z\s.]+$", ErrorMessage = "*Email address should be in the format adc@domain.com")]
-        [Display(Name = "Registered Email Address")]
+        [Display(Name = "*Registered Email Address")]
         public string email { get; set; }
         public bool emailSent { get; set; }
     }
